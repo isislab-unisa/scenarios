b79862bf26bb51c395a613206f008868b1450173
ddevin96 <danieledevinco1996@gmail.com>
2026-04-20 10:19:00 +0200

    fix: updated description

--- exam-playground/intro.md
+++ exam-playground/intro.md
@@ -2,8 +2,12 @@ Your first single container deployment
 
 You will be presented with a terminal and a browser window. The terminal is connected to a Kubernetes cluster where you can run `kubectl` commands. The browser window is connected to the cluster's API server, allowing you to access services running in the cluster.
 
+
+The background script will setup a templates folder in ~ folder, where you can find yaml files corresponding to each task in the book.
+
 To verify that the cluster is up and running, run the following command in the terminal: 
-You can che
+You can check that with the following command:
+
 ```bash
 kubectl wait --timeout=5m -n nginx-gateway \
     deployment/nginx-gateway \
@@ -14,6 +18,8 @@ kubectl wait --timeout=5m -n nginx-gateway \
 kubectl get deployment -n nginx-gateway
 kubectl get gatewayclass
 ```
+
+
 
 Click start to begin the scenario.
 
--- exam-playground/setup.sh
+++ exam-playground/setup.sh
@@ -1,3 +1,9 @@
+curl -L https://github.com/isislab-unisa/scenarios/archive/main.zip -o scenarios.zip
+unzip scenarios.zip -d scenarios
+mkdir templates
+mv scenarios/scenarios-main/kubernetes-system-design/* templates/
+rm -rf scenarios.zip scenarios/scenarios-main scenarios
+
 kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.2.0/standard-install.yaml
 kubectl apply -f https://raw.githubusercontent.com/nginx/nginx-gateway-fabric/v1.6.2/deploy/crds.yaml
 kubectl apply -f https://raw.githubusercontent.com/nginx/nginx-gateway-fabric/v1.6.2/deploy/default/deploy.yaml
@@ -5,9 +11,3 @@ kubectl apply -f https://raw.githubusercontent.com/nginx/nginx-gateway-fabric/v1
 kubectl wait --timeout=5m -n nginx-gateway \
     deployment/nginx-gateway \
     --for=condition=Available
-
-curl -L https://github.com/isislab-unisa/scenarios/archive/main.zip -o scenarios.zip
-unzip scenarios.zip -d scenarios
-mkdir templates
-mv scenarios/scenarios-main/kubernetes-system-design/* templates/
-rm -rf scenarios.zip scenarios/scenarios-main scenarios
